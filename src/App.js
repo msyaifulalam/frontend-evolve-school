e51b6bd1d5310333f90f88aeae26da3739cdb0a9
RiyNov <riyandi.novian@ymail.com>
2019-01-14 20:41:26 +0700

    First push by Riy

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import { Provider } from 'react-redux';
+//import store from './store';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import {
+  BrowserRouter,  //or 'BrowserRouter as Router,'
+  Route,
+  Switch
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      // <Provider store={store}>
+        <BrowserRouter>
+         {/* <div> */}
+            <Switch>
+              <Route exact path="/" component = {() => <Home/>}></Route>
+              <Route component = {() => <NotFound/>}></Route>
+            </Switch>
+          {/* </div> */}
+        </BrowserRouter>
+      //</Provider>
+      );
+    }
   }
-}
 
 export default App;
