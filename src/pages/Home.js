8ea92e0a36841467d2e911c2618e215ece45ae35
sendyfrandika <sendy.frandika@gmail.com>
2019-01-21 20:46:22 +0700

    footer revisi 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Footer from '../components/Footer/Footer';
-import Tanggalbeli from '../components/DaftarPembelian/Tanggalbeli';
 
 class Home extends Component {
     render() {
         return (
             <div className="App">
-                {/* <Tanggalbeli/> */}
                 <Footer/>
             </div>
         );
